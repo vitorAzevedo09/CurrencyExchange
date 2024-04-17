9e9cb160ee708c0a9f7c476816e26f3a78c05fe2
Vitor Silva <vitor.azevedo009@gmail.com>
2024-04-17 14:31:12 -0300

    Fixing junit pom and added mockito

---
+++ src/test/java/br/com/alura/currencyexchange/MainTest.java
@@ -0,0 +1,12 @@
+package br.com.alura.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @Test
+  private void testd_requesting_currencies() {
+
+  }
+
+}
--- test/java/br/com/alura/currencyexchange/MainTest.java
+++
@@ -1,5 +0,0 @@
-package br.com.alura.currencyexchange;
-
-class MainTest {
-
-}
